0bf0cead4640c9b91145f551dbafa428b414d2cd
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-09-28 12:41:53 -0700

    routes.js

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
-
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
 var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+
+// Sets up the Express App
+// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
-require('dotenv').config()
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(path.join(__dirname, '/public')));
 
 // Requiring our models for syncing
 var db = require("./models");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Import routes and give the server access to them.
-var indexRoutes = require("./controllers/webay_controller.js");
+// Static directory
+app.use(express.static("public"));
 
-app.use("/", indexRoutes);
+// Routes
+// =============================================================
+require("./routes/html-routes.js")(app);
+require("./routes/cutstomer-api-routes.js")(app);
+require("./routes/post-api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-    
+  });
 });
+
