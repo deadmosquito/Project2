4f71ddb82a3e4eff396113f308493212383cd920
Alex <alex@apresnail.com>
2019-09-28 11:22:49 -0700

    updated files

--- controllers/webay_controller.js
+++ controllers/webay_controller.js
@@ -9,64 +9,68 @@ var router = express.Router();
 var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.render("index");
 });
 
-router.get("/login", function(req, res){
-res.render("login")
+router.get("/login", function (req, res) {
+  res.render("login")
 });
 
-router.get("/register", function(req, res){
-    res.render("register")
-    });
+router.get("/register", function (req, res) {
+  res.render("register")
+});
+
+router.get("/cart", function (req, res) {
+  res.render("cart")
+});
 
 // Export routes for server.js to use
 
 var customer = require("../models/webay.js");
 
-router.get("/", function(req, res) {
-  
-    res.render("index");
+router.get("/", function (req, res) {
+
+  res.render("index");
 
 });
 
 router.post("/api/customer", function (req, res) {
-    customer.create(
-      ["fname", "lname", "phone", "address", "zip", "city", "country", "userpassword"],
-      [req.body.fname, req.body.lname, req.body.address, req.body.zip, req.body.city, req.body.country, req.body.password], 
-      function (result) {
-        res.json({ id: result.insertId });
-      });
-  });
-  
-  router.put("/api/customer/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-  
-    console.log("condition", condition);
-  
-    customer.update(
-      {
-        fname: req.body.fname,
-        lname: req.body.lname,
-        phone: req.body.phone,
-        address: req.body.address,
-        zip: req.body.zip,
-        city: req.body.city,
-        country: req.body.country,
-        userpassword: req.body.userpassword
-      },
-      condition,
-      function (result) {
-        if (result.changedRows === 0) {
-          return res.status(404).end();
-        }
-        res.status(200).end();
-  
+  customer.create(
+    ["fname", "lname", "phone", "address", "zip", "city", "country", "userpassword"],
+    [req.body.fname, req.body.lname, req.body.address, req.body.zip, req.body.city, req.body.country, req.body.password],
+    function (result) {
+      res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/customer/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  customer.update(
+    {
+      fname: req.body.fname,
+      lname: req.body.lname,
+      phone: req.body.phone,
+      address: req.body.address,
+      zip: req.body.zip,
+      city: req.body.city,
+      country: req.body.country,
+      userpassword: req.body.userpassword
+    },
+    condition,
+    function (result) {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
       }
-    );
-  });
-  
-  module.exports = router;
-  
+      res.status(200).end();
+
+    }
+  );
+});
+
+module.exports = router;
+
 
