ed2caaa843ff8dbb95bc6a078b21d533a048e93b
ashkan <ashkan.ram70@gmail.com>
2019-09-30 20:02:21 -0700

    updating

--- controllers/view-controller.js
+++ controllers/view-controller.js
@@ -34,19 +34,9 @@ router.get("/cart", function (req, res) {
   res.render('cart');
 });
 
+router.get("/login", function (req, res) {
+  res.render('login');
+});
 
-// helper for / and blog routes
-function renderBlog(req, res) {
-  var query = {};
-  if (req.query.author_id) {
-    query.AuthorId = req.query.author_id;
-  }
-  db.Post.findAll({
-    where: query,
-    include: [db.Author]
-  }).then(function (posts) {
-    res.render('blog', { posts: posts })
-  });
-}
 
 module.exports = router;
